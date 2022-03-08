56d7de6a2ac75d4006966fad093e97959fd2c585
Eugene Dubovik <dev.dubovik@gmail.com>
2022-03-08 22:03:31 +0300

    throws exception in signature

--- src/main/java/com/epam/jwd/logic/validator/Validator.java
+++ src/main/java/com/epam/jwd/logic/validator/Validator.java
@@ -1,5 +1,13 @@
 package com.epam.jwd.logic.validator;
 
+import com.epam.jwd.exception.InvalidEmailException;
+import com.epam.jwd.exception.InvalidNameException;
+import com.epam.jwd.exception.InvalidPasswordException;
+import com.epam.jwd.exception.InvalidPhoneNumberFormatException;
+
 public interface Validator<T> {
-    void validate(T t);
+    void validate(T t) throws InvalidNameException,
+            InvalidEmailException,
+            InvalidPhoneNumberFormatException,
+            InvalidPasswordException;
 }
