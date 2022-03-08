fc9192445906a831d54ab71ef8675608d72c6411
Eugene Dubovik <dev.dubovik@gmail.com>
2022-03-08 22:00:59 +0300

    create Exception for phone number validation

---
+++ src/main/java/com/epam/jwd/exception/InvalidPhoneNumberFormatException.java
@@ -0,0 +1,7 @@
+package com.epam.jwd.exception;
+
+public class InvalidPhoneNumberFormatException extends Exception {
+    public InvalidPhoneNumberFormatException(String message) {
+        super(message);
+    }
+}
